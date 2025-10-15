1d4fd4f220869a884fe4b865477b43a52414cf14
victor pagola <victorpagola.w@gmail.com>
2025-10-15 10:44:56 +0200

    new: exercise 11 done

--- src/App.java
+++ src/App.java
@@ -48,6 +48,7 @@ public class App {
         }
 
         System.err.printf("%n");
+
 // EJERCICIO 3
     System.err.println(ut.GREEN_BOLD + "EJERCICIO 3" + ut.RESET);
     System.err.println("""
@@ -103,6 +104,8 @@ public class App {
         } catch (Exception e) {
             System.err.println("Error inesperado!");
         }
+        System.err.printf("%n");
+
 // EJERCICIO 6
     System.err.println(ut.GREEN_BOLD + "EJERCICIO 6" + ut.RESET);
     System.err.println("""
@@ -121,22 +124,34 @@ public class App {
         } catch (Exception e) {
             System.err.println("ERROR! Error inesperado!");
         }
+        System.err.printf("%n%n");
+
 // EJERCICIO 9
-    System.err.println(ut.GREEN_BOLD + "EJERCICIO 9" + ut.RESET);
-    System.err.println("""
-            Realiza un programa que resuelva una ecuación de segundo grado 
-            (del tipo ax2 + bx + c = 0).
-            """);
-        try {
-            System.err.println("Por favor, introduzca los valores.");  
+        System.err.println(ut.GREEN_BOLD + "EJERCICIO 9" + ut.RESET);
+        System.err.println("""
+                Realiza un programa que resuelva una ecuación de segundo grado 
+                (del tipo ax2 + bx + c = 0).
+                """);
+            System.err.println("Por favor, introduzca los valores.");
+            System.err.print("a = ");
             double a = s.nextDouble();
+            System.err.print("b = ");
             double b = s.nextDouble();
+            System.err.print("c = ");
             double c = s.nextDouble();
+            double discriminante = Math.pow(b, 2) - 4*a*c;
+            
+            if (Double.isNaN(discriminante)) {
+                System.err.println("El número es nulo");
+            } else if (discriminante < 0) {
+                System.err.println("No hay soluciones reales.");
+            } else if (discriminante > 0) {
+                System.err.printf("x1 = %f%nx2 = %f", (-b + Math.sqrt(discriminante))/(2*a), (-b - Math.sqrt(discriminante))/(2*a) );
+            } else if (discriminante == 0) {
+                System.err.printf("x = %f", -b / (2*a));
+            } else 
             System.err.printf("");
-        } catch (NumberFormatException e) {
-            System.err.println("Número incorrecto");
-        }
+                }
 
 
-    }
 }
